5386aecde33b28ac1e1ab8f0375f0acc12394d4c
Viktor Ćirić <133128439+viktorciric@users.noreply.github.com>
2023-07-29 14:33:42 +0200

    Update admin_dashboard.php

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -16,7 +16,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <link rel="stylesheet" href="https://unpkg.com/dropzone@5/dist/min/dropzone.min.css" type="text/css" />
-    <link rel="stylesheet" href="admin_dash.css">
     <title>Admin Dashboard</title>
 </head>
 
